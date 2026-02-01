c02a25df258f392f42fff0bb68a7194fa830d778
Zeynuks <leznin2000@ya.ru>
2026-02-01 17:21:10 +0300

    Начал выполнение html_decode

---
+++ lab-2/html_decode/main.cpp
@@ -0,0 +1,15 @@
+#include "html_decode.h"
+
+int main()
+{
+	try
+	{
+	}
+	catch (const std::exception& errorMessage)
+	{
+		std::cerr << "ERROR: " << errorMessage.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
