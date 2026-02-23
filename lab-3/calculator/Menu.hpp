c987738f91458828d37bd3a853cd635ee8ff7fd1
Zeynuks <leznin2000@ya.ru>
2026-02-23 19:53:35 +0300

    Разбил Menu

--- lab-3/calculator/Menu.hpp
+++ lab-3/calculator/Menu.hpp
@@ -12,41 +12,16 @@
 class Menu
 {
 public:
-	void AddItem(const std::string& shortcut, const std::string& description,
-		std::unique_ptr<ICommand>&& command)
-	{
-		m_items.emplace_back(shortcut, description, std::move(command));
-	}
-	void Run()
-	{
-		std::string command;
-		while (std::cout << "> " && getline(std::cin, command) && ExecuteCommand(command))
-		{
-		}
-	}
-	void ShowInstructions() const
-	{
-		std::cout << "Commands list:" << std::endl;
-		for (auto& item : m_items)
-		{
-			std::cout << "  " << item.shortcut << ": " << item.description << std::endl;
-		}
-	}
-	void Exit()
-	{
-		m_exit = true;
-	}
+	void AddItem(const std::string& shortcut, const std::string& description, std::unique_ptr<ICommand>&& command);
+	void Run();
+	void ShowInstructions() const;
+	void Exit();
 
 private:
 	struct Item
 	{
-		Item(std::string shortcut, std::string description,
-			std::unique_ptr<ICommand>&& command)
-			: shortcut(std::move(shortcut))
-			, description(std::move(description))
-			, command(std::move(command))
-		{
-		}
+		Item(std::string shortcut, std::string description, std::unique_ptr<ICommand>&& command);
+
 		std::string shortcut;
 		std::string description;
 		std::unique_ptr<ICommand> command;
@@ -55,47 +30,6 @@ private:
 	std::vector<Item> m_items;
 	bool m_exit = false;
 
-	static std::pair<std::string, std::vector<std::string>> ParseLine(const std::string& line)
-	{
-		std::istringstream iss(line);
-		std::string shortcut;
-		iss >> shortcut;
-
-		std::vector<std::string> args;
-		std::string arg;
-		while (iss >> arg)
-			args.push_back(arg);
-
-		return { shortcut, args };
-	}
-
-	bool ExecuteCommand(const std::string& line)
-	{
-		m_exit = false;
-
-		auto [shortcut, args] = ParseLine(line);
-
-		const auto it = std::ranges::find_if(m_items, [&](const Item& item) {
-			return item.shortcut == shortcut;
-		});
-
-		if (it != m_items.end())
-		{
-			try
-			{
-				it->command->ParseArgs(args);
-				it->command->Execute();
-			}
-			catch (const std::exception& e)
-			{
-				std::cout << "Error: " << e.what() << std::endl;
-			}
-		}
-		else
-		{
-			std::cout << "Unknown command" << std::endl;
-		}
-
-		return !m_exit;
-	}
+	static std::pair<std::string, std::vector<std::string>> ParseLine(const std::string& line);
+	bool ExecuteCommand(const std::string& line);
 };
