2b6c0caac22e7616b433e2ad8038a62280c84d32
Zeynuks <leznin2000@ya.ru>
2026-04-02 12:24:17 +0300

    Выполнил правки

--- lab-5/my_string/MyString.hpp
+++ lab-5/my_string/MyString.hpp
@@ -146,7 +146,6 @@ public:
 	}
 
 	// оператор конкатенации строки MyString и стандартной строки std::string
-	//Почему нужен friend
 	[[nodiscard]] friend MyString operator+(MyString lhs, const std::string& rhs)
 	{
 		lhs += rhs;
